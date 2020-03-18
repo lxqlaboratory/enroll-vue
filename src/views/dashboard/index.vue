d00270f9dae0f5dcac2fb8ea76645908053c4efb
wenxue11 <714876076@qq.com>
2020-03-18 12:17:05 +0800

    f

---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="dashboard-container">
+    <div class="portlet-title">
+      <div class="caption font-darkblue bold" style=" text-align: center;">
+        <span>系统通知</span>
+      </div>
+    </div>
+
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Dashboard',
+  data() {
+    return {
+      noticeList: ''
+    }
+  },
+  created() {
+    this.fetchData()
+  },
+  methods: {
+    fetchData() {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.dashboard {
+  &-container {
+    margin: 30px;
+  }
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+  }
+}
+</style>
